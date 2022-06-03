8a0aa84aa78af1a2ceb7f98461fc759d863c0822
mapl <placenti.marco@gmail.com>
2022-06-03 16:09:44 +0200

    trying to adjust final layer

--- project-1.1/src/models/architectures.py
+++ project-1.1/src/models/architectures.py
@@ -6,19 +6,16 @@ import torch.nn.functional as F
 class ConvNet(nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.conv1 = nn.Conv2d(3, 16, 3, 1)
         self.dropout1 = nn.Dropout2d(0.25)
         self.dropout2 = nn.Dropout2d(0.5)
         self.fc1 = nn.Linear(246016, 128)
         self.fc2 = nn.Linear(128, 2)
 
-        self.log_softmax = nn.LogSoftmax()
+        self.log_softmax = nn.LogSoftmax(1)
 
     def forward(self, x):
         x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
         x = F.relu(x)
         x = F.max_pool2d(x, 2)
         x = self.dropout1(x)
