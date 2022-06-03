a60a36e1380b864e9b4480cdc49004a57f911a12
mapl <placenti.marco@gmail.com>
2022-06-03 15:18:25 +0200

    first commit to test code on hpc

---
+++ project-1.1/src/main.py
@@ -0,0 +1,20 @@
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import os
+
+from data.data_prep import Hotdog_NotHotdog
+
+
+
+if __name__ == "__main__":
+    size = 128
+    train_transform = transforms.Compose([transforms.Resize((size, size)), 
+                                        transforms.ToTensor()])
+    test_transform = transforms.Compose([transforms.Resize((size, size)), 
+                                        transforms.ToTensor()])
+
+    batch_size = 64
+    trainset = Hotdog_NotHotdog(train=True, transform=train_transform, data_path=os.path.abspath("/dtu/datasets1/02514/hotdog_nothotdog/train"))
+    train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=3)
+    testset = Hotdog_NotHotdog(train=False, transform=test_transform, data_path=os.path.abspath("/dtu/datasets1/02514/hotdog_nothotdog/test"))
+    test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=3)
---
+++ project-1.1/submit.sh
@@ -0,0 +1,11 @@
+02514sh
+
+module load python3/3.9.10
+
+python3 -m venv .
+
+source bin/activate
+
+pip install -r requirements.txt
+
+python3 src/main.py
