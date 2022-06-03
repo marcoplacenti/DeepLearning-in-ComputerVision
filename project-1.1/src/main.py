c45eff6f34ec3129ac9bc6dc862790aa87397ce8
mapl <placenti.marco@gmail.com>
2022-06-03 20:45:28 +0200

    cross-class performances

--- project-1.1/src/main.py
+++ project-1.1/src/main.py
@@ -4,6 +4,7 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
 import numpy as np
+from sklearn.metrics import confusion_matrix
 
 from data.data_prep import Hotdog_NotHotdog
 from models.architectures import *
@@ -17,9 +18,9 @@ def data_preparation():
                                         transforms.ToTensor()])
 
     batch_size = 64
-    trainset = Hotdog_NotHotdog(train=True, transform=train_transform)
+    trainset = Hotdog_NotHotdog(train=True, transform=train_transform, data_path='dtu/datasets1/02514/hotdog_nothotdog/')
     train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=3)
-    testset = Hotdog_NotHotdog(train=False, transform=test_transform)
+    testset = Hotdog_NotHotdog(train=False, transform=test_transform, data_path='dtu/datasets1/02514/hotdog_nothotdog/')
     test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=3)
 
     return trainset, train_loader, testset, test_loader
@@ -40,6 +41,17 @@ def train(model, loss_func, train_loader, optimizer, epoch, device):
         if batch_idx % 20 == 0:
             print(f"Epoch {epoch} Iteration {batch_idx}/{len(train_loader)}: Loss = {loss}")
 
+def performance_metrics(predictions, labels):
+    outcome = np.argmax(predictions, axis=1)
+
+    print(confusion_matrix(labels, outcome))
+
+    test_accuracy =  np.sum([1 if item == labels[idx] else 0 for idx, item in enumerate(outcome)])/len(outcome)
+
+
+
+    print(f"\nTest Accuracy: {round(test_accuracy,3)}\n")
+
 
 if __name__ == "__main__":
 
@@ -53,12 +65,13 @@ if __name__ == "__main__":
     
     loss_func = nn.NLLLoss()
 
-    for epoch in range(1, 25+1):
+    for epoch in range(1, 5+1):
         train(model, loss_func, train_loader, optimizer, epoch, device)
 
-    correct_preds = 0
+    predictions, agg_labels = [], []
     for batch_idx, (data, labels) in enumerate(test_loader):
         data, labels = data.to(device), labels.to(device)
+        agg_labels.extend(labels)
 
         model.eval()
         optimizer.zero_grad()
@@ -69,12 +82,10 @@ if __name__ == "__main__":
         if batch_idx % 20 == 0:
             print(f"Iteration {batch_idx}/{len(test_loader)}: Loss = {loss}")
 
-        preds = torch.exp(preds)
-        outcome = np.argmax(preds.cpu().detach().numpy(), axis=1)
-        correct_preds +=  np.sum([1 if item == labels[idx] else 0 for idx, item in enumerate(outcome)])
-
-    print(f"\nTest Accuracy: {round(correct_preds/len(testset),3)}\n")
+        predictions.extend(torch.exp(preds).cpu().detach().numpy())
+    
+    performance_metrics(predictions, agg_labels)
 
     print("Saving model weights and optimizer...")
-    torch.save(model.state_dict(), './src/models/model_final.pt')
-    torch.save(optimizer.state_dict(), './src/models/optim_final.pt')
+    torch.save(model.state_dict(), './models/model_final.pt')
+    torch.save(optimizer.state_dict(), './models/optim_final.pt')
