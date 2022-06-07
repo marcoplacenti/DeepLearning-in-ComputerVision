35584840467eb14fd0cefaefc9c967f839a463c9
mapl <placenti.marco@gmail.com>
2022-06-07 12:18:28 +0200

    training both VGG and Network sequentially

--- project-1.1/src/main.py
+++ project-1.1/src/main.py
@@ -5,6 +5,7 @@ from torch.utils.data import DataLoader
 
 import numpy as np
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import KFold
 
 from data.data_prep import Hotdog_NotHotdog
 from models.architectures import *
@@ -49,7 +50,26 @@ def data_preparation():
     trainset_5 = Hotdog_NotHotdog(train=True, transform=train_transforms_5)
     concat_dataset = torch.utils.data.ConcatDataset([trainset, trainset_1, trainset_2, trainset_3, trainset_4, trainset_5])
     
+    kfold = KFold(n_splits=5, shuffle=False)
+    trainloader_list, valloader_list = [], []
+    for (train_ids, val_ids) in kfold.split(concat_dataset):
+        train_subsampler = torch.utils.data.SubsetRandomSampler(train_ids)
+        val_subsampler = torch.utils.data.SubsetRandomSampler(val_ids)
+
+        trainloader_list.append(torch.utils.data.DataLoader(
+                        concat_dataset, 
+                        batch_size=batch_size,
+                        sampler=train_subsampler,
+                        num_workers=1))
+
+        valloader_list.append(torch.utils.data.DataLoader(
+                        concat_dataset,
+                        batch_size=batch_size,
+                        sampler=val_subsampler,
+                        num_workers=1))
+
     train_loader = DataLoader(concat_dataset, batch_size=batch_size, shuffle=True, num_workers=3)
+    
     testset = Hotdog_NotHotdog(train=False, transform=test_transform)#, data_path='dtu/datasets1/02514/hotdog_nothotdog/')
     test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=3)
 
@@ -88,32 +108,37 @@ if __name__ == "__main__":
     device = (torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'))
     print(f"Running on {device}")
     
-    model = VGG(input_channels=3, num_classes=2).to(device)
-    optimizer = optim.Adam(model.parameters(), lr=0.005)
+    models = [
+        Network(input_channels=3, num_classes=2).to(device),
+        VGG(input_channels=3, num_classes=2).to(device)
+    ]
+    for model in models:
+        optimizer = optim.Adam(model.parameters(), lr=0.005)
     
-    loss_func = nn.NLLLoss()
+        loss_func = nn.NLLLoss()
 
-    for epoch in range(1, 25+1):
-        train(model, loss_func, train_loader, optimizer, epoch, device)
+        for epoch in range(1, 25+1):
+            train(model, loss_func, train_loader, optimizer, epoch, device)
 
-    predictions, agg_labels = [], []
-    for batch_idx, (data, labels) in enumerate(test_loader):
-        data, labels = data.to(device), labels.to(device)
+        predictions, agg_labels = [], []
+        for batch_idx, (data, labels) in enumerate(test_loader):
+            data, labels = data.to(device), labels.to(device)
+            
+            model.eval()
+            optimizer.zero_grad()
+            preds = model(data)
+
+            loss = loss_func(preds, labels)
+
+            if batch_idx % 20 == 0:
+                print(f"Iteration {batch_idx}/{len(test_loader)}: Loss = {loss}")
+
+            agg_labels.extend(labels.cpu().detach().numpy())
+            predictions.extend(torch.exp(preds).cpu().detach().numpy())
         
-        model.eval()
-        optimizer.zero_grad()
-        preds = model(data)
+        performance_metrics(predictions, agg_labels)
 
-        loss = loss_func(preds, labels)
-
-        if batch_idx % 20 == 0:
-            print(f"Iteration {batch_idx}/{len(test_loader)}: Loss = {loss}")
-
-        agg_labels.extend(labels.cpu().detach().numpy())
-        predictions.extend(torch.exp(preds).cpu().detach().numpy())
-    
-    performance_metrics(predictions, agg_labels)
-
-    print("Saving model weights and optimizer...")
-    torch.save(model.state_dict(), './models/model_final.pt')
-    torch.save(optimizer.state_dict(), './models/optim_final.pt')
+        
+        print("Saving model weights and optimizer...")
+        torch.save(model.state_dict(), './models/model_final.pt')
+        torch.save(optimizer.state_dict(), './models/optim_final.pt')
