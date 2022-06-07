71b7b4f7f4acf9ffe5a0d9ffedf7c50b841923da
mapl <placenti.marco@gmail.com>
2022-06-07 14:00:52 +0200

    working cross validation

--- project-1.1/src/main.py
+++ project-1.1/src/main.py
@@ -2,6 +2,7 @@ import torch
 import torch.optim as optim
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
+import torch.nn.functional as F
 
 import numpy as np
 from sklearn.metrics import confusion_matrix
@@ -11,72 +12,63 @@ from data.data_prep import Hotdog_NotHotdog
 from models.architectures import *
 
 torch.manual_seed(42)
+np.random.seed(77)
+
+IMG_RESOLUTION = 128
+
+CROSS_VALIDATION = True
+K_SPLITS = 5
+
+DATA_AUGMMENTATION = True
+
+EPOCHS = 2
+LR = 0.005
+BATCH_SIZE = 64
+
 
 def data_preparation():
-    size = 128
-    train_transform = transforms.Compose([transforms.Resize((size, size)), 
+    
+    train_transform = transforms.Compose([transforms.Resize((IMG_RESOLUTION, IMG_RESOLUTION)), 
                                         transforms.ToTensor()])
-    test_transform = transforms.Compose([transforms.Resize((size, size)), 
+    train_dataset = Hotdog_NotHotdog(train=True, transform=train_transform, data_path='dtu/datasets1/02514/hotdog_nothotdog/')
+    
+    test_transform = transforms.Compose([transforms.Resize((IMG_RESOLUTION, IMG_RESOLUTION)), 
                                         transforms.ToTensor()])
+    testset = Hotdog_NotHotdog(train=False, transform=test_transform, data_path='dtu/datasets1/02514/hotdog_nothotdog/')
 
-    train_transforms_1 = transforms.Compose([transforms.Resize((size, size)),
-                                         transforms.RandomRotation((90,90)),
-                                         transforms.ToTensor(),
-                                        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-    train_transforms_2 = transforms.Compose([transforms.Resize((size, size)),
-                                            transforms.RandomRotation((180,180)),
+    if DATA_AUGMMENTATION:
+        train_transforms_1 = transforms.Compose([transforms.Resize((IMG_RESOLUTION, IMG_RESOLUTION)),
+                                            transforms.RandomRotation((90,90)),
                                             transforms.ToTensor(),
                                             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-    train_transforms_3 = transforms.Compose([transforms.Resize((size, size)),
-                                            transforms.RandomRotation((270,270)),
-                                            transforms.ToTensor(),
-                                            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-    train_transforms_4 = transforms.Compose([transforms.Resize((size, size)),
-                                            transforms.RandomHorizontalFlip(p=1.0),
-                                            transforms.ToTensor(),
-                                            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-    train_transforms_5 = transforms.Compose([transforms.Resize((size, size)),
-                                            transforms.RandomCrop(size=(128, 128)),
-                                            transforms.ToTensor(),
-                                            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+        train_transforms_2 = transforms.Compose([transforms.Resize((IMG_RESOLUTION, IMG_RESOLUTION)),
+                                                transforms.RandomRotation((180,180)),
+                                                transforms.ToTensor(),
+                                                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+        train_transforms_3 = transforms.Compose([transforms.Resize((IMG_RESOLUTION, IMG_RESOLUTION)),
+                                                transforms.RandomRotation((270,270)),
+                                                transforms.ToTensor(),
+                                                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+        train_transforms_4 = transforms.Compose([transforms.Resize((IMG_RESOLUTION, IMG_RESOLUTION)),
+                                                transforms.RandomHorizontalFlip(p=1.0),
+                                                transforms.ToTensor(),
+                                                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+        train_transforms_5 = transforms.Compose([transforms.Resize((IMG_RESOLUTION, IMG_RESOLUTION)),
+                                                transforms.RandomCrop(size=(IMG_RESOLUTION, IMG_RESOLUTION)),
+                                                transforms.ToTensor(),
+                                                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
-    batch_size = 64
+        trainset_1 = Hotdog_NotHotdog(train=True, transform=train_transforms_1)
+        trainset_2 = Hotdog_NotHotdog(train=True, transform=train_transforms_2)
+        trainset_3 = Hotdog_NotHotdog(train=True, transform=train_transforms_3)
+        trainset_4 = Hotdog_NotHotdog(train=True, transform=train_transforms_4)
+        trainset_5 = Hotdog_NotHotdog(train=True, transform=train_transforms_5)
+        train_dataset = torch.utils.data.ConcatDataset([train_dataset, trainset_1, trainset_2, trainset_3, trainset_4, trainset_5])
     
-    trainset = Hotdog_NotHotdog(train=True, transform=train_transform)#), data_path='dtu/datasets1/02514/hotdog_nothotdog/')
-    trainset_1 = Hotdog_NotHotdog(train=True, transform=train_transforms_1)
-    trainset_2 = Hotdog_NotHotdog(train=True, transform=train_transforms_2)
-    trainset_3 = Hotdog_NotHotdog(train=True, transform=train_transforms_3)
-    trainset_4 = Hotdog_NotHotdog(train=True, transform=train_transforms_4)
-    trainset_5 = Hotdog_NotHotdog(train=True, transform=train_transforms_5)
-    concat_dataset = torch.utils.data.ConcatDataset([trainset, trainset_1, trainset_2, trainset_3, trainset_4, trainset_5])
-    
-    kfold = KFold(n_splits=5, shuffle=False)
-    trainloader_list, valloader_list = [], []
-    for (train_ids, val_ids) in kfold.split(concat_dataset):
-        train_subsampler = torch.utils.data.SubsetRandomSampler(train_ids)
-        val_subsampler = torch.utils.data.SubsetRandomSampler(val_ids)
-
-        trainloader_list.append(torch.utils.data.DataLoader(
-                        concat_dataset, 
-                        batch_size=batch_size,
-                        sampler=train_subsampler,
-                        num_workers=1))
-
-        valloader_list.append(torch.utils.data.DataLoader(
-                        concat_dataset,
-                        batch_size=batch_size,
-                        sampler=val_subsampler,
-                        num_workers=1))
-
-    train_loader = DataLoader(concat_dataset, batch_size=batch_size, shuffle=True, num_workers=3)
-    
-    testset = Hotdog_NotHotdog(train=False, transform=test_transform)#, data_path='dtu/datasets1/02514/hotdog_nothotdog/')
-    test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=3)
-
-    return trainset, train_loader, testset, test_loader
+    return train_dataset, testset
 
 def train(model, loss_func, train_loader, optimizer, epoch, device):
-    
+    agg_labels, predictions = [], []
     for batch_idx, (data, labels) in enumerate(train_loader):
         data, labels = data.to(device), labels.to(device)
 
@@ -84,61 +76,117 @@ def train(model, loss_func, train_loader, optimizer, epoch, device):
         optimizer.zero_grad()
         output = model(data)
 
-        loss = loss_func(output, labels)
+        loss = loss_func(output, labels.unsqueeze(1).to(torch.float32).to(device))
 
         loss.backward()
         optimizer.step()
+
+        agg_labels.extend(labels.cpu().detach().numpy())
+        predictions.extend(torch.exp(output).cpu().detach().numpy())
         if batch_idx % 20 == 0:
             print(f"Epoch {epoch} Iteration {batch_idx}/{len(train_loader)}: Loss = {loss}")
 
-def performance_metrics(predictions, labels):
+def validate(model, loss_func, val_loader, optimizer, device):
+    predictions, agg_labels = [], []
+    for batch_idx, (data, labels) in enumerate(val_loader):
+        data, labels = data.to(device), labels.to(device)
+        
+        model.eval()
+        optimizer.zero_grad()
+        preds = model(data)
+
+        loss = loss_func(preds, labels)
+
+        if batch_idx % 20 == 0:
+            print(f"Iteration {batch_idx}/{len(val_loader)}: Loss = {loss}")
+
+        agg_labels.extend(labels.cpu().detach().numpy())
+        predictions.extend(torch.exp(preds).cpu().detach().numpy())
+
+    accuracy = performance_metrics(predictions, agg_labels, 'fold')
+    return model, accuracy
+
+def test(model, test_loader, optimizer):
+    predictions, agg_labels = [], []
+    for batch_idx, (data, labels) in enumerate(test_loader):
+        data, labels = data.to(device), labels.to(device)
+        
+        model.eval()
+        optimizer.zero_grad()
+        preds = model(data)
+
+        loss = loss_func(preds, labels)
+
+        if batch_idx % 20 == 0:
+            print(f"Iteration {batch_idx}/{len(test_loader)}: Loss = {loss}")
+
+        agg_labels.extend(labels.cpu().detach().numpy())
+        predictions.extend(torch.exp(preds).cpu().detach().numpy())
+    
+    accuracy = performance_metrics(predictions, agg_labels, 'test')
+
+def performance_metrics(predictions, labels, fold):
     outcome = np.argmax(predictions, axis=1)
-
     print(confusion_matrix(labels, outcome))
-
     test_accuracy =  np.sum([1 if item == labels[idx] else 0 for idx, item in enumerate(outcome)])/len(outcome)
-
-    print(f"\nTest Accuracy: {round(test_accuracy,3)}\n")
+    print(f"\n{fold} accuracy: {round(test_accuracy,3)}\n")
+    return test_accuracy
 
 
 if __name__ == "__main__":
 
-    trainset, train_loader, testset, test_loader = data_preparation()
+    train_dataset, testset = data_preparation()
 
+    trainloaders_list, valloaders_list = [], []
+    if CROSS_VALIDATION:
+        kfold = KFold(n_splits=K_SPLITS, shuffle=False)
+        for (train_ids, val_ids) in kfold.split(train_dataset):
+            train_subsampler = torch.utils.data.SubsetRandomSampler(train_ids)
+            val_subsampler = torch.utils.data.SubsetRandomSampler(val_ids)
+
+            trainloaders_list.append(torch.utils.data.DataLoader(
+                            train_dataset, 
+                            batch_size=BATCH_SIZE,
+                            sampler=train_subsampler,
+                            num_workers=3))
+
+            valloaders_list.append(torch.utils.data.DataLoader(
+                            train_dataset,
+                            batch_size=BATCH_SIZE,
+                            sampler=val_subsampler,
+                            num_workers=3))
+
+    else:
+        trainloaders_list.append(DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=3))
+        
+    test_loader = DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False, num_workers=3)
+    
     device = (torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'))
     print(f"Running on {device}")
     
-    models = [
-        Network(input_channels=3, num_classes=2).to(device),
-        VGG(input_channels=3, num_classes=2).to(device)
-    ]
-    for model in models:
-        optimizer = optim.Adam(model.parameters(), lr=0.005)
-    
-        loss_func = nn.NLLLoss()
-
-        for epoch in range(1, 25+1):
-            train(model, loss_func, train_loader, optimizer, epoch, device)
-
-        predictions, agg_labels = [], []
-        for batch_idx, (data, labels) in enumerate(test_loader):
-            data, labels = data.to(device), labels.to(device)
+    if CROSS_VALIDATION:
+        models_accuracies = {}
+        for i, train_loader in enumerate(trainloaders_list):
+            model = Network()
+            optimizer = optim.Adam(model.parameters(), lr=LR)
+            loss_func = nn.BCELoss() #loss_func = nn.NLLLoss()
             
-            model.eval()
-            optimizer.zero_grad()
-            preds = model(data)
+            for epoch in range(1, EPOCHS):
+                train(model, loss_func, train_loader, optimizer, epoch, device)
+            
+            kf_model, accuracy = validate(model, loss_func, valloaders_list[i], optimizer, device)
+            models_accuracies[accuracy] = kf_model
 
-            loss = loss_func(preds, labels)
+            final_model = [models_accuracies[key] for key in sorted(models_accuracies.keys(), reverse=True)]
+    else:
+        final_model = Network()
+        optimizer = optim.Adam(final_model.parameters(), lr=LR)
+        loss_func = nn.NLLLoss()
+        for epoch in range(1, EPOCHS):
+            train(final_model, loss_func, trainloaders_list, optimizer, epoch, device)
 
-            if batch_idx % 20 == 0:
-                print(f"Iteration {batch_idx}/{len(test_loader)}: Loss = {loss}")
+    test(final_model, test_loader, optimizer)
 
-            agg_labels.extend(labels.cpu().detach().numpy())
-            predictions.extend(torch.exp(preds).cpu().detach().numpy())
-        
-        performance_metrics(predictions, agg_labels)
-
-        
-        print("Saving model weights and optimizer...")
-        torch.save(model.state_dict(), './models/model_final.pt')
-        torch.save(optimizer.state_dict(), './models/optim_final.pt')
+    print("Saving model weights and optimizer...")
+    torch.save(final_model.state_dict(), './models/model_final.pt')
+    torch.save(optimizer.state_dict(), './models/optim_final.pt')
