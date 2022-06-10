5832201ccc129165d979519600557e6a793cbe16
mapl <placenti.marco@gmail.com>
2022-06-10 13:04:33 +0200

    split and multiprocessing

--- project-1.2/src/main.py
+++ project-1.2/src/main.py
@@ -3,7 +3,7 @@ import os
 import json
 from copy import deepcopy
 
-from multiprocessing import Pool
+from multiprocessing import Pool, Process, Manager
 from functools import partial
 
 import cv2
@@ -145,85 +145,90 @@ def crop_images_to_proposals(filepath, prop, new_image_size, img_annots):
     return cropped_resized_images
         
 
-data_images = []
-data_labels = []
-
 def process_image(file, data_dir, dataset):
-    global data_images, data_labels
     file_name = file['file_name']
+    print(file_name)
     img_annots = get_image_ground_truth(dataset, file_name)
     prop = get_image_proposals(data_dir + file_name, img_annots)
     prop_categories = assign_category_to_proposal(prop, img_annots)
     cropped_resized_images = crop_images_to_proposals(data_dir + file_name, prop, cropped_image_size, img_annots)
     cropped_resized_images_ground_truth = crop_images_to_proposals(data_dir + file_name,img_annots['bbox'], cropped_image_size, img_annots)
     
-    data_images = data_images + cropped_resized_images + cropped_resized_images_ground_truth
-    data_labels = data_labels + prop_categories + img_annots['supercategory']
+    img = cropped_resized_images + cropped_resized_images_ground_truth
+    label = train_labels + prop_categories + img_annots['supercategory']
 
-    """
-    print(f"Processing {file}...")
-    image = cv2.imread(data_dir+file)
-    image = cv2.resize(image, (224, 224))
-    
-    boxes = np.array(selective_search(image, mode='single', random_sort=False), dtype='object')
-    data_dir.split('/')[2]
-    np.save(f"./data/proposals/{batch_idx}/{file.split('.')[0]}", boxes)
-    """
-
-    """
-    patches = []
-    for box in boxes:
-        patch = image[box[0]:box[2], box[1]:box[3], :]
-        patch_res = cv2.resize(patch, (32, 32))
-        patches.append(patch_res)
-    
-    patches = np.array(patches, dtype='object')
-    np.save(f"./data/proposals/{file.split('.')[0]}", patches)
-    """
+    return img, label
 
 
 if __name__ == '__main__':
 
-    file_name = 'batch_1/000028.jpg'
-
-    data_dir = '/dtu/datasets1/02514/data_wastedetection/'
-    #data_dir = './data/'
+    #data_dir = '/dtu/datasets1/02514/data_wastedetection/'
+    data_dir = './data/'
     anns_file_path = data_dir + 'annotations.json'
 
     # Read annotations
     with open(anns_file_path, 'r') as f:
         dataset = json.loads(f.read())
 
+    dataset_size = len(dataset['images'])
+    train_dataset_size = int(0.8*dataset_size)
+    validation_dataset_size = int(0.1*dataset_size)
+    test_dataset_size = dataset_size - (train_dataset_size + validation_dataset_size)
+
+    arr = np.arange(0,dataset_size)
+    np.random.shuffle(arr)
+
+    train_dataset_id = arr[0:train_dataset_size]
+    validation_dataset_id = arr[train_dataset_size:train_dataset_size+validation_dataset_size]
+    test_dataset_id = arr[train_dataset_size+validation_dataset_size:train_dataset_size+validation_dataset_size+test_dataset_size]
+
+    train_set, val_set, test_set = [], [], []
+    for m in range(dataset_size):
+        candidate = dataset['images'][m]
+        if candidate['id'] in train_dataset_id:
+            train_set.append(candidate)
+        elif candidate['id'] in validation_dataset_id:
+            val_set.append(candidate)
+        elif candidate['id'] in test_dataset_id:
+            test_set.append(candidate)
+
     with Pool(processes=2) as pool:
         func = partial(process_image, data_dir=data_dir, dataset=dataset)
-        pool.map(func, dataset['images'])
+        vals = pool.map(func, train_set)
+        train_images = [pair[0] for pair in vals]
+        train_labels = [pair[1] for pair in vals]
+    
+    with Pool(processes=2) as pool:
+        func = partial(process_image, data_dir=data_dir, dataset=dataset, set_name='val')
+        vals = pool.map(func, val_set)
+        val_images = [pair[0] for pair in vals]
+        val_labels = [pair[1] for pair in vals]
 
-    sss = StratifiedShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
-    splits = sss.split(data_images, data_labels)
-    x_train_samples, x_test_samples = [], []
-    y_train_samples, y_test_samples = [], []
-    for train_index, test_index in splits:
-        x_train_samples.append(data_images[train_index]) 
-        x_test_samples.append(data_images[test_index])
-        y_train_samples.append(data_labels[train_index])
-        y_test_samples.append(data_labels[test_index])
+    with Pool(processes=2) as pool:
+        func = partial(process_image, data_dir=data_dir, dataset=dataset, set_name='test')
+        vals = pool.map(func, test_set)
+        test_images = [pair[0] for pair in vals]
+        test_labels = [pair[1] for pair in vals]
+    
+    if not os.path.exists('./data/split_dataset'):
+        os.makedirs('./data/split_dataset')
 
+    train_images = np.array(train_images, dtype='object')
+    np.save('./data/split_dataset/train_images.npy', train_images)
 
-    """
-    for batch in range(15):
-        print(f"Searching for bounding boxes in batch {batch+1}...")
-        data_dir = f'./data/batch_{batch+1}/'
-        
-        if not os.path.exists('./data/proposals/'):
-            os.makedirs('./data/proposals/')
+    train_labels = np.array(train_labels, dtype='object')
+    np.save('./data/split_dataset/train_labels.npy', train_labels)
 
-        if not os.path.exists(f'./data/proposals/{batch+1}/'):
-            os.makedirs(f'./data/proposals/{batch+1}/')
+    val_images = np.array(val_images, dtype='object')
+    np.save('./data/split_dataset/val_images.npy', val_images)
 
-        files = os.listdir(data_dir)
-        
-        with Pool(processes=4) as pool:
-            func = partial(process_image, data_dir=data_dir, batch_idx=batch+1)
-            pool.map(func, files)
-    """
+    val_labels = np.array(val_labels, dtype='object')
+    np.save('./data/split_dataset/val_labels.npy', val_labels)
+
+    test_images = np.array(test_images, dtype='object')
+    np.save('./data/split_dataset/test_images.npy', test_images)
+
+    test_labels = np.array(test_labels, dtype='object')
+    np.save('./data/split_dataset/test_labels.npy', test_labels)
+
                 
