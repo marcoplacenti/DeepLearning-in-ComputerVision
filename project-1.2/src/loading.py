44dfc0a4f986423f198ff941d0c2d4d143641575
mapl <placenti.marco@gmail.com>
2022-06-11 15:28:11 +0200

    fixed error

--- project-1.2/src/loading.py
+++ project-1.2/src/loading.py
@@ -12,7 +12,10 @@ def get_dataloader(set_name):
     dir = os.listdir(f'./data/split_dataset/{set_name}/')
     dir_len = len([d for d in dir if d.startswith(set_name+'_image')])
     if set_name == 'train':
-        dir_len = int(dir_len*0.4)
+        dir_len = int(dir_len*0.30)
+
+    if set_name == 'val':
+        dir_len = int(dir_len*0.50)
 
     data_sep = []
     data, labels = [], []
@@ -39,6 +42,6 @@ def get_dataloader(set_name):
 
     return dataloader
 
-val_loader = get_dataloader('val')
+#val_loader = get_dataloader('val')
 #test_loader = get_dataloader('test')
 train_loader = get_dataloader('train')
