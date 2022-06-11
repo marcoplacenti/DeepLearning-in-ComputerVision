e19072c0f69f68a5633fb8d678c435d200505431
mapl <placenti.marco@gmail.com>
2022-06-11 12:13:03 +0200

    testing loading of saved pickle sample file

--- project-1.2/src/loading.py
+++ project-1.2/src/loading.py
@@ -17,4 +17,38 @@ val_labels = joblib.load('./data/split_dataset/val/val_labels.pkl')
 
 val_filenames = joblib.load('./data/split_dataset/val_filenames.pkl')
 
-print(val_data)
+print("done")
+
+
+imgs = os.listdir('./data/split_dataset/test/')
+
+val_data_sep = []
+val_data = []
+for idx, img in tqdm(enumerate(imgs)):
+    if img.startswith('test_image'):
+        img_proposals = joblib.load('./data/split_dataset/test/'+img)
+        val_data_sep.append(img_proposals)
+        val_data.extend(img_proposals)
+
+val_labels = joblib.load('./data/split_dataset/test/test_labels.pkl')
+
+val_filenames = joblib.load('./data/split_dataset/test_filenames.pkl')
+
+print("done")
+
+
+imgs = os.listdir('./data/split_dataset/train/')
+
+val_data_sep = []
+val_data = []
+for idx, img in tqdm(enumerate(imgs)):
+    if img.startswith('train_image'):
+        img_proposals = joblib.load('./data/split_dataset/train/'+img)
+        val_data_sep.append(img_proposals)
+        val_data.extend(img_proposals)
+
+val_labels = joblib.load('./data/split_dataset/train/train_labels.pkl')
+
+val_filenames = joblib.load('./data/split_dataset/train_filenames.pkl')
+
+print("done")
