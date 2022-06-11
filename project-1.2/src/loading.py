23b5a3ad8d940d6bba38e65bb49e492e63276112
mapl <placenti.marco@gmail.com>
2022-06-11 14:51:13 +0200

    fixed error

--- project-1.2/src/loading.py
+++ project-1.2/src/loading.py
@@ -10,20 +10,20 @@ import numpy as np
 imgs = os.listdir('./data/split_dataset/val/')
 
 val_data_sep = []
-val_data = []
+val_data, val_labels = [], []
 for idx, img in tqdm(enumerate(imgs)):
     if img.startswith('val_image') and idx < 10:
         img_proposals = joblib.load('./data/split_dataset/val/'+img)
-       
         val_data_sep.append(img_proposals)
-
         val_data.extend(img_proposals)
+    if img.startswith('val_labels') and idx < 10:
+        lab_proposals = joblib.load('./data/split_dataset/val/'+img)
+        val_labels.extend(lab_proposals)
         
-val_labels = joblib.load('./data/split_dataset/val/val_labels.pkl')
 classes_map = dict(zip(set(val_labels), range(len(val_labels))))
 val_labels = np.array([np.array([int(classes_map[label])]) for label in val_labels])
 
-val_filenames = joblib.load('./data/split_dataset/val_filenames.pkl')
+#val_filenames = joblib.load('./data/split_dataset/val_filenames.pkl')
 
 val_data = np.array(val_data, dtype=np.float32)
 tensor_x = torch.Tensor(val_data) # transform to torch tensor
